60176a1324c79a1582582331753904ce31743e2d
sekisuri <sekisuri@gmail.com>
2019-06-19 02:16:34 +0900

    dotween path

---
+++ Assets/01.Chaman Cat/08.Scripts/Dotween/Ex01.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+/*
+* @author sekisuri
+* http://ottcat.com
+*/
+public class Ex01 : MonoBehaviour 
+{
+
+
+    [Button(ButtonSizes.Large)]
+    public void Tween01()
+    {
+        transform.DOSpiral(1, Vector3.forward, SpiralMode.ExpandThenContract, 1, 10);
+    }
+
+
+}
