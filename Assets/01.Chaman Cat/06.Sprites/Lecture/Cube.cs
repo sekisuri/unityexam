1f4fb7ee9d07798a44d6f8f4e3ef53c83cb713c3
sekisuri <sekisuri@gmail.com>
2019-07-03 07:14:20 +0900

    연습

---
+++ Assets/01.Chaman Cat/06.Sprites/Lecture/Cube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    Vector3 rotation;
+    private void Start()
+    {
+        rotation = this.transform.eulerAngles;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Rotate(new Vector3(90, 0, 0) * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/01.Chaman Cat/08.Scripts/Csharp/DelegateExam.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+* @author sekisuri
+* http://ottcat.com
+*/
+delegate int MyDelegate(int a, int b);
+
+class Calculator
+{
+    public int Plus(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Minus(int a, int b)
+    {
+        return a - b;
+    }
+}
+public class DelegateExam : MonoBehaviour 
+{
+
+    private void Start()
+    {
+        Calculator Calc = new Calculator();
+        MyDelegate Callback;
+        Callback = new MyDelegate(Calc.Plus);
+
+#if true
+        Debug.Log("Callback(3,4) : " + Callback(3,4));
+#endif
+
+        Callback = new MyDelegate(Calculator.Minus);
+
+#if true
+        Debug.Log("Callback(7,5) : " + Callback(5,7) );
+#endif
+
+
+    }
+
+}
