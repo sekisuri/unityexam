1a30e437720add3dacbadfd0ea3fe0d9f0c2f10a
sekisuri <sekisuri@gmail.com>
2019-07-31 14:46:25 +0900

    no message

---
+++ Assets/02.RubyAdventure/Scripts/RubyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+#if true
+        Debug.Log(horizontal);
+#endif
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
+        transform.position = position;
+
+    }
+}
