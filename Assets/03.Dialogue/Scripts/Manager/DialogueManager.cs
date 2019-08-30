1c2abf03da712eadd5ebfa3e7df198e937b4d3d5
sekisuri <sekisuri@gmail.com>
2019-08-30 20:19:22 +0900

    대화창에 이름 나타내기

--- Assets/03.Dialogue/Scripts/Manager/DialogueManager.cs
+++ Assets/03.Dialogue/Scripts/Manager/DialogueManager.cs
@@ -14,21 +14,91 @@ public class DialogueManager : MonoBehaviour
     [SerializeField] GameObject interEvent;
     Dialogue[] dialogues;
 
-    bool isDialogue = false;
+    bool isDialogue = false; // 대화중일 경우 true.
+    bool isNext = false; // 특정 키 입력 대기.
+
+    int lineCount = 0;
+    int contextCount = 0;
 
     private void Start()
     {
         ShowDialogue(interEvent.transform.GetComponent<InteractionEvent>().GetDialogue());
         
     }
+    private void Update()
+    {
+        if (isDialogue)
+        {
+            if (isNext)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    isNext = false;
+                    txt_Dialogue.text = "";
+                    
+                    contextCount++;
+                    if(contextCount < dialogues[lineCount].contexts.Length)
+                    {
+                        StartCoroutine(TypeWriter());
+                    }
+                    else
+                    {
+                        contextCount = 0;
+                        lineCount++;
+                        if(lineCount < dialogues.Length)
+                        {
+                            StartCoroutine(TypeWriter());
+                        }
+                        else
+                        {
+                            EndDialogue();
+                        }
+                    }
+
+#if true
+                    Debug.Log("Space ");
+#endif
+
+                    
+                    
+                }
+            }
+        }
+    }
+
+    void EndDialogue()
+    {
+        isDialogue = false;
+        contextCount = 0;
+        lineCount = 0;
+        dialogues = null;
+        isNext = false;
+        SettingUI(false);
+    }
     public void ShowDialogue(Dialogue[] p_dialogues)
     {
+        isDialogue = true;
         txt_Dialogue.text = "";
         txt_Name.text = "";
         dialogues = p_dialogues;
-        SettingUI(true);
+        //SettingUI(true);
+        StartCoroutine(TypeWriter());
     }
 
+    IEnumerator TypeWriter()
+    {
+        string t_ReplaceText = dialogues[lineCount].contexts[contextCount];
+        t_ReplaceText = t_ReplaceText.Replace("`", ",");
+
+#if true
+        Debug.Log("대화 : " + t_ReplaceText);
+#endif
+
+        txt_Dialogue.text = t_ReplaceText;
+        txt_Name.text = dialogues[lineCount].name;
+        isNext = true;
+        yield return null;
+    }
     void SettingUI(bool p_flag)
     {
         go_DialogueBar.SetActive(p_flag);
