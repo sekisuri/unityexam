f9fd18919d6c5aa11f05d72c0954370fd6a5a0b8
sekisuri - GitHub <sekisuri@gmail.com>
2019-09-13 03:39:12 +0900

    오브젝트 풀링

---
+++ Assets/04.ObjectPooling/CreateCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCube : MonoBehaviour
+{
+
+    private void Start()
+    {
+        StartCoroutine(CreateCoroutine());
+    }
+
+    IEnumerator CreateCoroutine()
+    {
+        while (true)
+        {
+            yield return null;
+            GameObject t_object = ObjectPoolingManager.instance.GetQueue();
+            t_object.transform.position = Vector3.zero;
+        }
+    }
+}
