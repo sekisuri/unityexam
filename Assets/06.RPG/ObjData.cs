e8f00034d22dc22fad8597db98b237c64ef40a17
sekisuri - GitHub <sekisuri@gmail.com>
2019-10-05 04:56:56 +0900

    rpg 대화 추가

---
+++ Assets/06.RPG/ObjData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjData : MonoBehaviour
+{
+    Dictionary<int, string[]> talkData;
+
+    private void Awake()
+    {
+        talkData = new Dictionary<int, string[]>();
+        GenerateData();
+
+    }
+
+    void GenerateData()
+    {
+        talkData.Add(1000, new string[] { "안녕?", "이 곳에 처음 왔구나?" });
+        talkData.Add(100, new string[] { "평범한 나무상자다." });
+        talkData.Add(200, new string[] { "누군가 사용했던 흔적이 있는 책상이다." });
+    }
+
+    public string GetTalk(int id, int talkIndex)
+    {
+        if(talkIndex == talkData[id].Length)
+        {
+            return null;
+        }
+        else
+        {
+            return talkData[id][talkIndex];
+        }
+    }
+
+
+}
---
+++ Assets/06.RPG/RpgManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RpgManager : MonoBehaviour
+{
+
+    public GameObject talkPanel;
+    public Text TalkText;
+    public GameObject scanObject;
+    public bool isAction;
+    public int talkIndex;
+
+    public void Action(GameObject scanObj)
+    {
+        if (isAction)
+        {
+            isAction = false;
+        }
+        else
+        {
+            isAction = true;
+            scanObject = scanObj;
+            TalkText.text = "이것의 이름은 " + scanObject.name + "이라고 한다.";
+        }
+        talkPanel.SetActive(isAction);
+
+    }
+
+    void Talk(int id , bool isNpc)
+    {
+        //    string talkData = RpgTalkManger.GetTalk(id, talkIndex);
+        string talkData ="";
+        if(talkData == null)
+        {
+            isAction = false;
+            talkIndex = 0;
+            return;
+        }
+        if (isNpc)
+        {
+            TalkText.text = talkData;
+        }
+        else
+        {
+            TalkText.text = talkData;
+        }
+        isAction = true;
+        talkIndex++;
+    }
+}
