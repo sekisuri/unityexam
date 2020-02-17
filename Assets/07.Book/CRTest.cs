3f9c13fa9240506345ce4433e4ebf20868c2a5a3
sekisuri - GitHub <sekisuri@gmail.com>
2020-02-18 03:50:08 +0900

    no message

---
+++ Assets/07.Book/CRTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CRTest : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine("TestCoroutine");
+    }
+
+    IEnumerator TestCoroutine()
+    {
+        Debug.Log("코루틴 시작");
+        yield return new WaitForSeconds(3.0f);
+        Debug.Log("3초 후 빠져나감");
+    }
+}
