d2ba5a8b24c2f465864361a9defc715d2d292789
sekisuri - GitHub <sekisuri@gmail.com>
2020-02-18 04:36:02 +0900

    cvs파일 읽기

---
+++ Assets/07.Book/newdata/LoadCvs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadCvs : MonoBehaviour
+{
+    public string all_data;
+    public string[] data_list;
+
+    private void Awake()
+    {
+        TextAsset TextF = (TextAsset)Resources.Load("gamedata") as TextAsset;
+        all_data = TextF.text;
+        data_list = all_data.Split('\n');
+    }
+
+    private void Start()
+    {
+        for (int i = 1; i < data_list.Length - 1; i++)
+        {
+            string[] splitedData = data_list[i].Split(',');
+            Debug.Log(splitedData[0] + " " + int.Parse(splitedData[1]) + " " + int.Parse(splitedData[2]) + " " + int.Parse(splitedData[3]));
+        }
+    }
+}
