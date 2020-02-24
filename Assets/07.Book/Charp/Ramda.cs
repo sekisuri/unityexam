9eb66dd3bbb33d8058c3296e2765b861475744f7
sekisuri - GitHub <sekisuri@gmail.com>
2020-02-25 03:37:07 +0900

    람다 예제

---
+++ Assets/07.Book/Charp/Ramda.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ramda : MonoBehaviour
+{
+    int a = 5;
+    int b = 5;
+
+    int sum;
+    delegate void MyDelegate();
+    MyDelegate myDelegate;
+    
+    private void Add()
+    {
+        sum = a + b;
+    }
+
+    //private void Print()
+    //{
+    //    print(sum);
+    //}
+
+    private void Back()
+    {
+        sum = 0;
+    }
+
+    private void Start()
+    {
+        myDelegate = Add;
+        //myDelegate += delegate () { print(sum); };
+        myDelegate += () => print(sum);
+        myDelegate += Back;
+
+        myDelegate();
+    }
+}
---
+++ Assets/07.Book/Charp/Ramda02.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ramda02 : MonoBehaviour
+{
+    delegate void MyDelegate<T>(T a, T b);
+    MyDelegate<string> myDelegate;
+
+    private void Start()
+    {
+        myDelegate += (string a, string b) => { print(b); print(a); };
+        myDelegate("Im here" ,"Man ");
+
+    }
+}
