6f382276e1bebe5f71c8eef985ebe73ddace5b41
sekisuri - GitHub <sekisuri@gmail.com>
2020-02-25 03:37:33 +0900

    형식매개변수

---
+++ Assets/07.Book/Charp/Tvalue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tvalue : MonoBehaviour
+{
+    void Print<T>(T value) 
+    {
+        print(value);
+    }
+
+    private void Start()
+    {
+        Print<string>("string abc");
+        Print<float>(4.4f);
+    }
+
+}
