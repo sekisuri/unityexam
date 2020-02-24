a40818e30fd845091c459a4ed0a64ab021ddacdd
sekisuri - GitHub <sekisuri@gmail.com>
2020-02-25 03:37:50 +0900

    action , frunc 예제

---
+++ Assets/07.Book/Charp/TAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TAction : MonoBehaviour
+{
+    delegate string MyDelegate<T1, T2>(T1 a, T2 b);
+    MyDelegate<int, int> myDelegate;
+
+    Action<int, int> myDelegate2;
+
+    Func<int, int, string> myDelegate3;
+
+    private void Start()
+    {
+        myDelegate3 = (int a, int b) =>
+        {
+            int sum = a + b;
+            return sum + "이 리턴되었습니다.";
+        };
+
+        print(myDelegate3(3, 5));
+    }
+}
